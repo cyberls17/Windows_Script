635dc95f5f01ebd563216c344f71e61ed6b568a6
cyberls17 <mhs0001@naver.com>
2022-06-25 09:15:53 +0900

    Add UAC Load

--- Oracle_DB_Service_Manager.bat
+++ Oracle_DB_Service_Manager.bat
@@ -1,5 +1,24 @@
 @echo off
 
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+ if '%errorlevel%' NEQ '0' (
+     echo Requesting administrative privileges...
+     goto UACReload
+ ) else ( goto GOT_UAC )
+
+:UACReload
+     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+     exit /B
+
+:GOT_UAC
+     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+     pushd "%CD%"
+     CD /D "%~dp0"
+
 echo ------------------------------
 echo Oracle Database Service Manager
 echo ------------------------------
